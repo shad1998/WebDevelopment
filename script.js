25ad971e1833211b0ffb3f069e7043cf9030428d
shad1998 <shad16cs@cmrit.ac.in>
2018-09-28 21:56:21 +0530

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+var fname=prompt("what is your first name?");
+var lname=prompt("what is your last name?");
+var age=prompt("what is your age?");
+// if(fname!=null and lname!=null)
+alert("your full name is: "+fname+" "+lname);
+alert("your age is "+age);
